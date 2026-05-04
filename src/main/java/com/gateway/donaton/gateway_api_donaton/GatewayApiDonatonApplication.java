e36f64604ee8e8bb860f5c3aceafc725b621c974
vfabar <vi.fabar@duocuc.cl>
2026-05-04 12:47:05 -0400

    api gateway donaton base

---
+++ src/main/java/com/gateway/donaton/gateway_api_donaton/GatewayApiDonatonApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.donaton.gateway_api_donaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiDonatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiDonatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/donaton/gateway_api_donaton/GatewayApiDonatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.donaton.gateway_api_donaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiDonatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
